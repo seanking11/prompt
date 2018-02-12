2f99ee2da10636b1687b62336b351c5366234fc3
Sean King <kingsc@miamioh.edu>
2018-02-11 20:18:22 -0500

    feat(App): Set up App component with store and firebase

--- App.js
+++ App.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import ReduxThunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise'
+import firebase from 'firebase'
+import LoginScreen from './src/components/LoginScreen'
+import reducers from './src/reducers'
 
-export default class App extends React.Component {
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk, promiseMiddleware))
+
+export default class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyBFB-hYLOOER4UNdOTgaws98v3oQKCZvPU',
+      authDomain: 'prompt-1efbf.firebaseapp.com',
+      databaseURL: 'https://prompt-1efbf.firebaseio.com',
+      projectId: 'prompt-1efbf',
+      storageBucket: 'prompt-1efbf.appspot.com',
+      messagingSenderId: '471922845764'
+    }
+
+    firebase.initializeApp(config)
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>This is the start of something great</Text>
-      </View>
+      <Provider store={store}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <LoginScreen />
+        </View>
+      </Provider>
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
