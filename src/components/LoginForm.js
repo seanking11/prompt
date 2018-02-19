d44e2d9eba6d2dd0bfd37bb1610ab9999588d7f4
Sean King <kingsc@miamioh.edu>
2018-02-18 23:27:00 -0500

    feat(LoginForm): Add mutations for login and sign up

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Text, Dimensions, View } from 'react-native'
-import { TextField } from 'react-native-material-textfield'
 import { Button, Card, InputItem, WhiteSpace } from 'antd-mobile'
+import { graphql, compose } from 'react-apollo'
 // import { Input, Spinner, Card, CardSection } from './common'
-import { emailChanged, passwordChanged, loginUser } from '../actions'
+import { emailChanged, passwordChanged, usernameChanged, loginUser } from '../actions'
+import createUserMutation from '../mutations/createUser'
+import loginUserMutation from '../mutations/loginUser'
 
 const WIDTH = Dimensions.get('window').width
 const MARGIN = 60
@@ -25,7 +27,31 @@ class LoginForm extends Component {
   onButtonPress = () => {
     const { email, password } = this.props
 
-    this.props.loginUser({ email, password })
+    const input = { email, password }
+
+    this.props.mutate({
+      variables: { input }
+      // refetchQueries: [{ query }]
+    }).catch(err => {
+      const errors = err.graphQLErrors.map(error => error.message)
+      console.log('error creating', err);
+      this.setState({ errors })
+    })
+  }
+
+  onCreateUserButtonPress = () => {
+    const { email, username, password } = this.props
+
+    const input = { email, username, password }
+
+    this.props.mutate({
+      variables: { input }
+      // refetchQueries: [{ query }]
+    }).catch(err => {
+      const errors = err.graphQLErrors.map(error => error.message)
+      console.log('error creating user:', err)
+      this.setState({ errors })
+    })
   }
 
   render() {
@@ -42,6 +68,16 @@ class LoginForm extends Component {
             onChange={phone => this.props.emailChanged(phone)}
             value={this.props.email}
             error={this.props.error.toLowerCase().indexOf('email') >= 0}
+          />
+
+          <WhiteSpace />
+
+          <InputItem
+            type='text'
+            placeholder='Username'
+            onChangeText={username => this.props.usernameChanged(username)}
+            value={this.props.username}
+            error={this.props.error.toLowerCase().indexOf('username') >= 0}
           />
 
           <WhiteSpace />
@@ -66,6 +102,15 @@ class LoginForm extends Component {
           >
             Login
           </Button>
+
+          <Button
+            primary
+            loading={this.props.loading}
+            onClick={this.onCreateUserButtonPress}
+            style={{ margin: 15 }}
+          >
+            Create User
+          </Button>
         </Card.Body>
       </Card>
     )
@@ -75,8 +120,13 @@ class LoginForm extends Component {
 const mapStateToProps = state => ({
   email: state.auth.email,
   password: state.auth.password,
+  username: state.auth.username,
   error: state.auth.error,
   loading: state.auth.loading
 })
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged, loginUser })(LoginForm)
+export default compose(
+  graphql(createUserMutation),
+  // graphql(loginUserMutation),
+  connect(mapStateToProps, { emailChanged, passwordChanged, usernameChanged, loginUser })
+)(LoginForm)
