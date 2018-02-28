1ca4c0287b695f7bfa2c336c41f5cb6f0769d004
Sean King <kingsc@miamioh.edu>
2018-02-28 13:49:49 -0500

    feat(FeedScreen): Add logout functionality

--- src/components/screens/FeedScreen.js
+++ src/components/screens/FeedScreen.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import { View, Text, AsyncStorage } from 'react-native'
 
+const logout = navigation => {
+  AsyncStorage.removeItem('token')
+  navigation.navigate('auth')
+}
+
 class FeedScreen extends Component {
-  static navigationOptions = {
+  static navigationOptions = ({ navigation }) => ({
     headerTitle: new Date().toLocaleDateString(),
-    headerLeft: <Text onPress={() => AsyncStorage.removeItem('token')}>Profile</Text>,
+    headerLeft: <Text onPress={() => logout(navigation)}>Logout</Text>,
     headerRight: <Text>Add</Text>
-  }
+  })
 
   render() {
     return (
