282356827e5486b9a116c4e339895635078acc13
Sean King <kingsc@miamioh.edu>
2018-03-10 14:03:49 -0500

    feat(Avatar): Add borderColor and pass through ...rest

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -1,10 +1,23 @@
 import React from 'react'
 import { Image } from 'react-native'
 
-const Avatar = ({ size = 50, img }) => (
+const Avatar = ({
+  borderColor,
+  img,
+  size = 50,
+  style,
+  ...rest
+}) => (
   <Image
-    style={{ height: size, width: size, borderRadius: size / 2 }}
+    style={[{
+      height: size,
+      width: size,
+      borderRadius: size / 2,
+      borderWidth: 2,
+      borderColor: borderColor || 'transparent'
+    }, style]}
     source={{ uri: img }}
+    {...rest}
   />
 )
 
