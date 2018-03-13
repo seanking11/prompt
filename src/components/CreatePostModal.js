a991b3f3b3fc7bbc91ce34895231256dfd4e59c8
Sean King <kingsc@miamioh.edu>
2018-03-13 16:41:55 -0400

    feat(CreatePostModal): Add ability to create a post

--- src/components/CreatePostModal.js
+++ src/components/CreatePostModal.js
@@ -4,20 +4,73 @@ import {
   Button,
   Image,
   Modal,
-  Dimensions
+  Dimensions,
+  Text
 } from 'react-native'
 import { InputItem } from 'antd-mobile'
+import { graphql } from 'react-apollo'
+import createPostMutation from '../mutations/createPost'
 import { DismissKeyboard } from './common'
+import config from '../config'
 
 const { width } = Dimensions.get('window')
 
-const DismissKeyboardView = DismissKeyboard(View)
+const DismissKeyboardView = DismissKeyboard(View) // eslint-disable-line new-cap
 
 class CreatePostModal extends Component {
   state = {
     caption: ''
   }
 
+  _onCreatePostButtonPress = (localUri) => {
+      const formData = new FormData()
+      const fileName = new Date().getTime().toString()
+      const data = {
+        uri: localUri,
+        name: `${fileName}.jpg`,
+        type: 'image/jpeg'
+      }
+      formData.append('data', data)
+
+      const options = {
+        method: 'POST',
+        body: formData,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'multipart/form-data'
+        }
+      }
+
+      fetch(config.fileUrl, options)
+        .then(response => {
+          console.log(response) // eslint-disable-line no-console
+          return response.json()
+        })
+        .then(image => {
+          this.setState({
+            uploadedImageId: image.id
+          })
+
+          const tempUserId = 'cje3ieb6nd2my0196xe1wf2qj'
+
+          const vars = {
+            caption: this.state.caption,
+            fileId: this.state.uploadedImageId,
+            userId: tempUserId, // grab from store later
+            file: {
+              name: `${this.state.uploadedImageId}-${tempUserId}`
+            }
+          }
+
+          this.props.mutate({ variables: vars })
+            .then(() => {
+              // Successfully created POST
+              this.props.closeModal()
+            })
+            .catch(err => console.log('Error creating post', err)) // eslint-disable-line no-console
+        })
+        .catch(err => console.log('Error uploading image', err)) // eslint-disable-line no-console
+    }
   render() {
     return (
       <Modal
@@ -36,6 +89,7 @@ class CreatePostModal extends Component {
             onChangeText={caption => this.setState({ caption })}
             value={this.state.caption}
           />
+          <Button title='Create Post' onPress={() => this._onCreatePostButtonPress(this.props.image.uri)} />
           <Button title='Close' onPress={() => this.props.closeModal()} />
         </DismissKeyboardView>
       </Modal>
@@ -43,4 +97,4 @@ class CreatePostModal extends Component {
   }
 }
 
-export default CreatePostModal
+export default graphql(createPostMutation)(CreatePostModal)
