9461821e7bd74ffc0b6efc3b70feb0a440403b64
Sean King <kingsc@miamioh.edu>
2018-04-02 15:15:25 -0400

    feat(Avatar): Have default image if not supplied

--- src/components/common/Avatar.js
+++ src/components/common/Avatar.js
@@ -7,18 +7,21 @@ const Avatar = ({
   size = 50,
   style,
   ...rest
-}) => (
-  <Image
-    style={[{
-      height: size,
-      width: size,
-      borderRadius: size / 2,
-      borderWidth: 2,
-      borderColor: borderColor || 'transparent'
-    }, style]}
-    source={{ uri: img }}
-    {...rest}
-  />
-)
+}) => {
+  const image = img ? { uri: img } : require('../../../assets/userPlaceholder.png')
+  return (
+    <Image
+      style={[{
+        height: size,
+        width: size,
+        borderRadius: size / 2,
+        borderWidth: 2,
+        borderColor: borderColor || 'transparent'
+      }, style]}
+      source={image}
+      {...rest}
+    />
+  )
+}
 
 export default Avatar
