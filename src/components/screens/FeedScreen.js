1417d0ac95ca014a54bf21a05f7a7bc027011a3d
Sean King <kingsc@miamioh.edu>
2018-04-06 14:44:06 -0400

    feat(ProfileScreen): Add avatar and base styling

--- src/components/screens/FeedScreen.js
+++ src/components/screens/FeedScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, AsyncStorage, TouchableOpacity, StatusBar } from 'react-native'
+import { View, TouchableOpacity, StatusBar } from 'react-native'
 import { ImagePicker } from 'expo'
 import Icon from 'react-native-vector-icons/FontAwesome'
 import { graphql } from 'react-apollo'
@@ -9,12 +9,6 @@ import { MyAppText } from '../common'
 import PostsList from '../PostsList'
 import CreatePostModal from '../CreatePostModal'
 import fetchPrompt from '../../queries/fetchPrompt'
-
-const logout = navigation => {
-  AsyncStorage.removeItem('token')
-  AsyncStorage.removeItem('loggedInUser')
-  navigation.navigate('auth')
-}
 
 const HeaderTitle = () => (
   <MyAppText style={{ fontFamily: 'ProximaNovaBold', fontSize: 20 }}>
@@ -27,7 +21,11 @@ class FeedScreen extends Component {
     const { params = {} } = navigation.state
     return {
       headerTitle: <HeaderTitle />,
-      headerLeft: <MyAppText onPress={() => navigation.navigate('profile')} style={{ marginLeft: 15 }}>Profile</MyAppText>,
+      headerLeft: (
+        <MyAppText onPress={() => navigation.navigate('profile')} style={{ marginLeft: 15 }}>
+          Profile
+        </MyAppText>
+      ),
       headerRight: (
         <TouchableOpacity onPress={() => params.savePhoto()}>
           <Icon name='plus' color='#000' size={20} style={{ marginRight: 15, padding: 5 }} />
--- src/components/screens/ProfileScreen.js
+++ src/components/screens/ProfileScreen.js
@@ -1,14 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { View, Dimensions, AsyncStorage, TouchableOpacity } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { MyAppText, Avatar } from '../common'
 
-export default () => (
-  <View>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-    <Text>ProfileScreen</Text>
-  </View>
-)
+const { width } = Dimensions.get('window')
+
+const styles = {
+  containerStyles: {
+    width,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  },
+  titleStyle: {
+    fontFamily: 'ProximaNovaBold',
+    fontSize: 20
+  }
+}
+
+const logout = navigation => {
+  AsyncStorage.removeItem('token')
+  AsyncStorage.removeItem('loggedInUser')
+  navigation.navigate('auth')
+}
+
+class ProfileScreen extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerRight: (
+      <TouchableOpacity onPress={() => logout(navigation)}>
+        <Icon name='sign-out' color='#000' size={20} style={{ marginRight: 15, padding: 5 }} />
+      </TouchableOpacity>
+    )
+  })
+
+  componentDidMount() {
+    console.log('Mounted. Props: ', this.props)
+  }
+
+  render() {
+    return (
+      <View style={styles.containerStyles}>
+        <Avatar
+          size={100}
+        />
+        <MyAppText style={styles.titleStyle}>Sean King</MyAppText>
+      </View>
+    )
+  }
+}
+
+export default ProfileScreen
