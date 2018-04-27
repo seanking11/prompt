b7d18082371ea344a6b9cd24c434de66171f836e
Sean King <kingsc@miamioh.edu>
2018-04-26 20:45:11 -0400

    feat(LikeButton): Add ability to like and unlike posts

--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 import { TouchableOpacity } from 'react-native'
+import { graphql, compose } from 'react-apollo'
+import { connect } from 'react-redux'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import createLikeMutation from '../mutations/createLike'
+import deleteLikeMutation from '../mutations/deleteLike'
+
 
 class LikeButton extends Component {
   state = {
-    liked: false
+    liked: this.props.likeId !== null,
+    likeId: this.props.likeId
   }
 
   _handleOnPress = () => {
+    if (this.state.liked) {
+      this.props.deleteLike({
+        variables: {
+          id: this.state.likeId
+        }
+      })
+      this.props.handleUnLike()
+    } else {
+      this.props.createLike({
+        variables: {
+          userId: this.props.user.id,
+          postId: this.props.postId
+        }
+      }).then(response => this.setState({ likeId: response.data.createLike.id }))
+      this.props.handleOnLike()
+    }
     this.setState({ liked: !this.state.liked })
   }
 
@@ -21,4 +43,12 @@ class LikeButton extends Component {
   }
 }
 
-export default LikeButton
+const mapStateToProps = state => ({
+  user: state.auth.user
+})
+
+export default compose(
+  graphql(createLikeMutation, { name: 'createLike' }),
+  graphql(deleteLikeMutation, { name: 'deleteLike' }),
+  connect(mapStateToProps, null)
+)(LikeButton)
