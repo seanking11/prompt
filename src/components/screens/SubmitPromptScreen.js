e95f00047694a86bc007de9cac27f7af3cc47564
Sean King <kingsc@miamioh.edu>
2018-05-13 17:48:14 -0400

    feat(Screens): Add SubmitPromptScreen component

---
+++ src/components/screens/SubmitPromptScreen.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import { MyAppText } from '../common'
+
+class SubmitPromptScreen extends Component {
+  render() {
+    return (
+      <View>
+        <MyAppText>Submit a prompt!!</MyAppText>
+        <MyAppText>Submit a prompt!!</MyAppText>
+        <MyAppText>Submit a prompt!!</MyAppText>
+        <MyAppText>Submit a prompt!!</MyAppText>
+        <MyAppText>Submit a prompt!!</MyAppText>
+      </View>
+    )
+  }
+}
+
+export default SubmitPromptScreen
